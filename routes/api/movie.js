d98029a2036bb50cfc16e92c443776a123f239e0
Mannex Pimentel <mannex247@gmail.com>
2020-02-02 00:39:41 -0500

    break

--- routes/api/movie.js
+++ routes/api/movie.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const Movie = require('../../models/Movie')
+const User = require('../../models/User')
 
 
-router.get('/', (req, res) => res.send('Movies route'));
+router.get('/', auth, async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(550).send('Server Error')
+    }
+});
 
 module.exports = router;
