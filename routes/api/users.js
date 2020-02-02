9ee341305e13e8a69785844019578ddae7f9e1b9
Mannex Pimentel <mannex247@gmail.com>
2020-02-01 23:25:11 -0500

    register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
+// Register User
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Pease include valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+    
 
-router.get('/', (req, res) => res.send('User route'));
+    res.send("User route");
+  }
+);
 
 module.exports = router;
