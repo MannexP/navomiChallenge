6305942706883d3c01c6b7887ce648cc9313a5fa
Mannex Pimentel <mannex247@gmail.com>
2020-02-02 01:39:19 -0500

    restart computer

--- models/Movie.js
+++ models/Movie.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 
 const MovieSchema = new mongoose.Schema({
 user: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
     ref:"user"
 },
 name: {
--- routes/api/movie.js
+++ routes/api/movie.js
@@ -1,18 +1,48 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const {check, validationResult } = require('express-validator');
 
 const Movie = require('../../models/Movie')
 const User = require('../../models/User')
 
 
-router.get('/', auth, async (req, res) => {
-    try {
-        
-    } catch (err) {
+router.post(
+    "/",
+    [
+      auth,
+      [
+        check("name", "Name is Required")
+          .not()
+          .isEmpty()
+      ]
+    ],
+    async (req, res) => {
+      
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+      
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      try {
+     
+        const user = await User.findById(req.user.id).select("-password");
+  
+      
+        const newMovie = new Movie({
+          name: req.body.name,
+          user: req.user.id
+        });
+  
+       
+        const movie = await newMovie.save();
+        res.json(movie);
+      } catch (err) {
         console.error(err.message);
-        res.status(550).send('Server Error')
+        res.status(500).send("Server Error");
+      }
     }
-});
+  );
 
 module.exports = router;
