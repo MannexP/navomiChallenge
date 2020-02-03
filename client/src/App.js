3926799b3e57f9749c509ac030791f8154750c23
Mannex Pimentel <mannex247@gmail.com>
2020-02-02 19:14:54 -0500

    alert and remove

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/layout/auth/Register";
 import Login from "./components/layout/auth/Login";
+import Alert from "./components/layout/Alert";
+
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -16,6 +18,7 @@ const App = () => (
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className="container">
+        <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
